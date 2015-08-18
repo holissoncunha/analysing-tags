44e0c6bab2cf43e69fe8bf0b26ba6243860de615
Holisson Cunha <holisson.cunha@gmail.com>
2015-08-18 08:58:26 -0300

    update file

--- similarity.py
+++ similarity.py
@@ -17,15 +17,17 @@ def buldingDB(db):
 # gera arquivo de saída
 def output(similarity,tag1,tag2,usuario,tp_tag):
 
-    outputFile.write('Usuário: ' + usuario +'\t'+ 'Foto: ' + tp_tag + '\n')
-    outputFile.write('Não recomendado: ' + tag2 + '\n')
-    outputFile.write('Recomendado: ' + tag1 + '\n')
-    outputFile.write('Similaridade: ' + str(similarity[0][0]) + '\n\n')
-    return True
+	string  = 'Usuário: ' + usuario +'\t'+ 'Foto: ' + tp_tag + '\n'
+	string += 'Não recomendado: ' + tag2 + '\n'
+	string += 'Recomendado: ' + tag1 + '\n'
+	string += 'Similaridade: ' + str(similarity[0][0]) + '\n\n'
+
+	outputFile.write(string)
+
+	return True
 
 # remove acento de todos as tags
 def removeAccents(tag):
-
     return unicodedata.normalize('NFD', unicode(tag, 'utf-8')).encode('ascii','ignore')
 
 # Analisa a similaridade entre as fotos, desconsiderando o usuário
@@ -37,30 +39,30 @@ def byPhoto():
 def byUser():
 
     for i in notRecommendedDB:
-        tag = []
-        for j in recommendedDB:
-            if i[0] == j[0]:                       # verifica se é o mesmo usuário
-                if i[2] == j[3]:                   # verifica se é a mesma foto
-                    tag.append(j[1].lower())
-        tag1 = removeAccents(' '.join(tag))
-        tag2 = removeAccents(i[1].lower())
-        tags = (tag1,tag2)
-        similarity = cosine(tags)
-        output(similarity,tag1,tag2,i[0],i[2])
+		tag = []
+		for j in recommendedDB:
+			if i[0] == j[0]:                       # verifica se é o mesmo usuário
+				if i[2] == j[3]:                   # verifica se é a mesma foto
+					tag.append(j[1].lower())
+		tag1 = removeAccents(' '.join(tag))
+		tag2 = removeAccents(i[1].lower())
+		tags = (tag1,tag2)
+		similarity = cosine(tags)
+		output(similarity,tag1,tag2,i[0],i[2])
 
     return None
 
 # cálcula a similaridade entre duas tags
 def cosine(tagSet):
 
-    count_vectorizer = CountVectorizer()
-    count_matrix = count_vectorizer.fit_transform(tagSet)
-    return cosine_similarity(count_matrix[0], count_matrix[1])
+	count_vectorizer = CountVectorizer()
+	count_matrix = count_vectorizer.fit_transform(tagSet)
+	return cosine_similarity(count_matrix[0], count_matrix[1])
 
 if __name__ == '__main__':
 
-    recommendedDB    = buldingDB('recommended.csv')
-    notRecommendedDB = buldingDB('not-recommended.csv')
+    recommendedDB    = buldingDB('files/recommended.csv')
+    notRecommendedDB = buldingDB('files/not-recommended.csv')
     byUser()
     #byPhoto()
     outputFile.close()
